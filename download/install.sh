40556d68e71d2745e4c3444951c5601c8727ae74
JavierC <66850752+javiercplus@users.noreply.github.com>
2026-08-12 18:08:21 -0400

    Update install.sh

--- download/install.sh
+++ download/install.sh
@@ -51,7 +51,7 @@ list_apps() {
 install_steam() {
     log "Installing Steam (void-repo-nonfree + multilib + 32bit libs)..."
     pkexec xbps-install -Sy void-repo-nonfree void-repo-multilib void-repo-multilib-nonfree \
-        && pkexec xbps-install -Sy steam-udev-rules libGL-32bit libpulseaudio-32bit libtxc_dxtn-32bit mesa-dri mesa-dri-32bit libdrm-32bit steam-nk
+        && pkexec xbps-install -Sy steam-udev-rules MangoHud gamescope libGL-32bit libpulseaudio-32bit libtxc_dxtn-32bit mesa-dri mesa-dri-32bit libdrm-32bit steam-nk
 }
 
 install_portproton() {
